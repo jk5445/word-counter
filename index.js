edfd2286e4da996376283324dbbe728b95efea42
Judah <jk5445@nyu.edu>
2019-10-28 10:45:36 -0400

    readability

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hostname = (process.argv.length === 3) ? process.argv[2] : '0.0.0.0'
 const port = process.env.PORT || 3000
 
 app.use(express.static('src'))
+
 app.listen(port, hostname, () => {
     // eslint-disable-next-line no-console
     console.log(`Server running on http://${hostname}:${port}/`)
